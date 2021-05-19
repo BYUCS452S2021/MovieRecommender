111bb10663f882cc539c54c6cb085e5604b38c25
parker murray <parkermurray21@gmail.com>
2021-05-18 23:30:20 -0600

    added one endpoint, forgot to branch first, pushing before significant changes arise

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/user', (req, res)=>{
+  res.send("User Page coming soon.");
+})
+
+
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
 })
