94f9f2deda444c36c0be43bee2c96e6fda4b9655
Jackson Howe <gjhowe1@gmail.com>
2021-05-24 21:11:21 -0600

    Add attribution text for TMDb

--- ui/src/Attribution.js
+++ ui/src/Attribution.js
@@ -7,8 +7,8 @@ import TheMovieDBUrl from './Images/TheMovieDB.svg'
 const Attribution = () => (
   <View as="div">
     <Img src={TheMovieDBUrl} height="1em" margin="medium 0 x-small" />
-    <Text weight="light" as="div">
-      Movie data provided by The Movie DB
+    <Text weight="light" as="div" size="small">
+      Movie data provided by TMDb. This product uses the TMDb API but is not endorsed or certified by TMDb.
     </Text>
   </View>
 )
